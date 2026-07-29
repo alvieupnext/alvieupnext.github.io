b970252d01c273ae1f0af9362d285414d7bb0c5e
alvieupnext <alvarovargasguerrero@hotmail.com>
2026-07-29 23:31:53 +0200

    implemented Paper channel and /latestpaper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing';
+import { LatestPaper } from './latestpaper/latestpaper';
 
 export const routes: Routes = [
   { path: 'landing', component: Landing },
+  { path: 'latestpaper', component: LatestPaper },
   { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy, signal, ViewChild, ElementRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })

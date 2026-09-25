db28016e066d368389fa3335915f9f2f59c6fe98
alvieupnext <alvarovargasguerrero@hotmail.com>
2026-09-26 01:03:06 +0200

    Add updates page and updates data

---
+++ updates-data.js
@@ -0,0 +1,26 @@
+const updatesData = [
+  {
+    id: "update-2026-09-26-iclr",
+    date: "2026-09-26T00:54:00",
+    displayDate: "26 Sep 2026",
+    message: "Submitted a paper to ICLR!"
+  },
+  {
+    id: "update-2026-09-26-cv",
+    date: "2026-09-26T00:30:00",
+    displayDate: "26 Sep 2026",
+    message: "Academic CV Updated"
+  },
+  {
+    id: "update-2026-07-15",
+    date: "2026-07-15T14:00:00",
+    displayDate: "15 Jul 2026",
+    message: "SR Federated Causal Discovery pre-print available"
+  },
+  {
+    id: "update-2026-05-01",
+    date: "2026-05-01T09:30:00",
+    displayDate: "1 May 2026",
+    message: "Beyond Weights available on arXiv"
+  }
+];
